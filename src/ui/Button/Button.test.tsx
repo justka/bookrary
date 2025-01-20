48c601e47a1e9003f688b0326316fb3894b66e11
Justyna <justka070391@gmail.com>
2025-01-20 22:51:33 +0100

    add more tests to component Button

--- src/ui/Button/Button.test.tsx
+++ src/ui/Button/Button.test.tsx
@@ -1,25 +1,51 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Button } from "./Button";
 
 test("Should render component Button without errors", () => {
-  // ARRANGE
   render(<Button text="" />);
 
-  // ACT
-
-  // ASSERT
   const button = screen.getByRole("button");
   expect(button).toBeTruthy();
 });
 
 test("Should render component Button with text on it", () => {
-  // ARRANGE
   render(<Button text="Submit" />);
 
-  // ACT
-
-  // ASSERT
   const button = screen.getByRole("button");
   expect(button).toHaveTextContent("Submit");
+});
+
+test("Should render component Button with type submit", () => {
+  render(
+    <Button
+      text="Submit"
+      type="submit"
+    />
+  );
+
+  const button = screen.getByRole("button");
+  expect(button).toHaveAttribute("type", "submit");
+});
+
+test("Should render component Button with type button", () => {
+  render(<Button text="Submit" />);
+
+  const button = screen.getByRole("button");
+  expect(button).toHaveAttribute("type", "button");
+});
+
+test("Should call onClick when button is clicked", () => {
+  const handleClick = jest.fn();
+  render(
+    <Button
+      onClick={handleClick}
+      text="Submit"
+    />
+  );
+
+  const button = screen.getByText("Submit");
+  fireEvent.click(button);
+
+  expect(handleClick).toHaveBeenCalledTimes(1);
 });
