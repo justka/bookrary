c82b92d93a15838371dc20ed43cb4723481f7042
Justyna <justka070391@gmail.com>
2025-04-15 21:35:15 +0200

    get random image

--- src/components/Lists/Books/components/ListContent/ListContent.tsx
+++ src/components/Lists/Books/components/ListContent/ListContent.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeBookFromList } from "store/booksSlice";
 import { Button } from "ui/Button/Button";
+import { gql, useQuery } from "@apollo/client";
 import {
   Alert,
   AlertTitle,
@@ -30,6 +31,23 @@ export function ListContent() {
     }) => state.books
   );
 
+  const GET_LOCATIONS = gql`
+    query GetLocations {
+      locations {
+        photo
+      }
+    }
+  `;
+  const { data, error, loading } = useQuery(GET_LOCATIONS);
+  const dataPrepared = data?.locations || [];
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error : {error.message}</p>;
+  }
+
   if (booksList.length === 0) {
     return (
       <Alert
@@ -44,13 +62,15 @@ export function ListContent() {
   return (
     <>
       {booksList.map((item, index, baseArray) => {
+        const randomImage =
+          dataPrepared[Math.floor(Math.random() * dataPrepared.length)].photo;
         return (
           <React.Fragment key={`${index}-${item.title}`}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
                 <Avatar
                   alt={item.title}
-                  src="/"
+                  src={randomImage}
                 />
               </ListItemAvatar>
               <ListItemText
